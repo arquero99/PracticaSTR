cf8958ca68b3f2a5593a2fe7eb5b07ef980c516e
Juan <j.arquerog@gmail.com>
2021-09-22 20:58:12 +0200

    Implementado ejercicio 4. Pendiente de compilación y depuración

--- seminarioADA/sesion7-tareas/lanzatareas.adb
+++ seminarioADA/sesion7-tareas/lanzatareas.adb
@@ -3,10 +3,71 @@ with Kernel.Serial_Output; use Kernel.Serial_Output;
 
 procedure lanzatareas is
 
+type Vector is array (1..50) of integer;
+
 --   pragma Priority (System.Priority'First);
+
+--Declaraciones
+
+procedure printVector ();
+
+
+Suma : Integer;
+Maximo : Integer;
 
 task A;
 task B;
+task Ordena;
+task SumaElementos;
+task getMaximo;
+
+
+-----------------
+--Tareas Implementadas
+------------------
+
+task body Ordena is
+	Temp : Integer;
+begin
+	for I in reverse Vector'Range loop
+	for J in Vector'First .. I loop
+		if A(I) < A(J) then
+			Temp := Vector(J);
+			Vector(J) := Vector(I);
+			Vector(I) := Temp;
+		end if;
+	end loop;
+	end loop;
+end Ordena;
+
+
+
+task body SumaElementos is
+begin
+	Suma:=0;
+	for I in Vector'Range loop
+		Suma:=Suma + Vector(I);
+	end loop;
+end SumaElementos;
+
+
+
+task body getMaximo is
+	Temp:Integer:=0;
+	for I in Vector'Range loop
+		if Vector(I)>Temp then
+			Temp:=Vector(I)
+		end if
+	end loop
+	Maximo:=Temp;
+end getMaximo;
+			
+
+-----------------
+--Tareas suministradas
+-----------------
+
+
 
 task body A is 
 begin
@@ -29,3 +90,20 @@ begin
    Put_Line ("adios");
 
 end lanzatareas;
+
+
+--------------
+--Procedimiento implementado
+--------------
+
+procedure printVector() is
+	New_Line;
+	Put ("Vector: ")
+	for I in Vector'Range loop
+		Put(Integer'Image(Vector(I)));
+		if I<Vector'Last then 
+		Put(", ");
+		end if;
+	New_Line;
+	end loop;
+end printVector;
