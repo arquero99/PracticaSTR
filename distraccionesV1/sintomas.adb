0c1cbffeaaaaa944573464a5567b63c4ef05e457
Juan <j.arquerog@gmail.com>
2021-10-26 19:59:19 +0200

    Añanadida implementación de objeto protegido sintomas. Primera prueba

--- distraccionesV1/sintomas.adb
+++ distraccionesV1/sintomas.adb
@@ -13,6 +13,26 @@ package body sintomas is
 			begin
 			sintomasProtegido.distActual=dist;
 		end setDistancia;
+		
+		procedure setVolante (vol: in boolean) is
+			begin
+			sintomasProtegido.volantazo=vol;
+		end setVolante;
+		
+		procedure getCabezaInclinada() is  
+			begin 
+			return sintomasProtegido.cabezaInclinada;
+		end getCabeza;
+		
+		procedure getDistancia() is
+			begin
+			return sintomasProtegido.distActual;
+		end getDistancia;
+
+		procedure getVolante() is
+			begin
+			return sintomasProtegido.volantazo;
+		end getVolante;
 	
 	end sintomasProtegido;
 begin
--- distraccionesV1/sintomas.ads
+++ distraccionesV1/sintomas.ads
@@ -2,10 +2,14 @@ package sintomas is
 
 	Protected sintomasProtegido is 
 		procedure setCabezaInclinada(pos: in boolean);
-		procedure setDistancia (dist: in situacionDist);
-
+		procedure setDistancia (dist: in situacionDist);	
+		procedure setVolante(vol: in boolean);						//Incluir geters
+		procedure getCabezaInclinada();
+		procedure getDistancia();							//Incluir variables para volante
+		procedure getVolante();
 		private
 			cabezaInclinada : boolean;
 			distActual : situacionDist;
+			volantazo: boolean;
 	end sintomasProtegido;
 end sintomas;
