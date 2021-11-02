30edba5981c1eca99286fb2b1e53834845096853
Juan <j.arquerog@gmail.com>
2021-11-02 21:09:15 +0100

    Añadido parámetro en objeto medidas para almacenar velociad

--- distraccionesV1/medidas.adb
+++ distraccionesV1/medidas.adb
@@ -11,6 +11,17 @@ package body medidas is
 			begin
 			return medidasProtegido.distMetros;
 		end getDistancia;
+		
+		procedure setVelocidad (vel in: Speed_Samples_Type) is
+			begin
+			medidasProtegido.velocidad=vel;
+		end setVelocidad;
+
+		function getVelocidad return Speed_Samples_Type is 
+			begin
+			return medidasProtegido.velocidad;
+		end getDistancia;
+
 	end medidasProtegido;
 end medidas;
 
--- distraccionesV1/medidas.ads
+++ distraccionesV1/medidas.ads
@@ -3,8 +3,9 @@ with devices use devices;
 package medidas is
 	Protected medidasProtegido is
 		procedure setDistancia(dist in: Distance_Samples_Type);			//Creados métodos y objeto usando tipo distancia declarado en devices.ads
-		function getDistancia return Distance_Samples_Type;
+		function getDistancia return Distance_Samples_TypeDis;
 		private 
-			distMetros : Distance_Samples_Type; 
+			distMetros : Distance_Samples_Type;
+			velocidad :  Speed_Samples_Type;
 	end medidasProtegido;
 end medidas;
