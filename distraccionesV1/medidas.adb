628f40039a2fba4d85171b13f3da21323a7fa2ad
Juan <j.arquerog@gmail.com>
2021-11-14 16:58:54 +0100

    Arreglados algunos errores

--- distraccionesV1/medidas.adb
+++ distraccionesV1/medidas.adb
@@ -1,10 +1,11 @@
 with devices use devices;
 
 package body medidas is 
-	Protected medidasProtegido is 
-		procedure setDistancia(dist in: Distance_Samples_Type) is
+
+	Protected body medidasProtegido is 
+		procedure setDistancia(dist : in Distance_Samples_Type) is
 			begin
-			medidasProtegido.distMetros=dist;
+			medidasProtegido.distMetros:=dist;
 		end setDistancia;
 
 		function getDistancia return Distance_Samples_Type is 
@@ -12,15 +13,18 @@ package body medidas is
 			return medidasProtegido.distMetros;
 		end getDistancia;
 		
-		procedure setVelocidad (vel in: Speed_Samples_Type) is
+		procedure setVelocidad (vel : in Speed_Samples_Type) is
 			begin
-			medidasProtegido.velocidad=vel;
+			medidasProtegido.velocidad:=vel;
 		end setVelocidad;
 
 		function getVelocidad return Speed_Samples_Type is 
 			begin
 			return medidasProtegido.velocidad;
 		end getDistancia;
+	private 
+		distMetros: Distance_Samples_Type;
+		velocidad: Speed_Samples_Type;
 
 	end medidasProtegido;
 end medidas;
--- distraccionesV1/medidas.ads
+++ distraccionesV1/medidas.ads
@@ -1,10 +1,10 @@
-with devices use devices;
+with devices; use devices;
 
 package medidas is
 	Protected medidasProtegido is
-		procedure setDistancia(dist in: Distance_Samples_Type);			--Creados métodos y objeto usando tipo distancia y velocidad declarado en devices.ads
-		function getDistancia return Distance_Samples_TypeDis;
-		procedure setVelocidad(vel in: Speed_Samples_Type);
+		procedure setDistancia (dist : in Distance_Samples_Type);			--Creados métodos y objeto usando tipo distancia y velocidad declarado en devices.ads
+		function getDistancia return Distance_Samples_Type;
+		procedure setVelocidad(vel : in Speed_Samples_Type);
 		function getVelocidad return Speed_Samples_Type;
 		private 
 			distMetros : Distance_Samples_Type;
